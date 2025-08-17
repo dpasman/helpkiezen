d8961844a41665c88c0fa6aa24d4dadf6c27b822
dpasman <demi-pasman@hotmail.com>
2025-08-17 20:29:18 +0200

    chore: ignore OS/artifact files and remove Zone.Identifier

--- src/utils/analytics.js
+++
@@ -1,8 +0,0 @@
-// English code as requested
-export function trackOutbound(label) {
-  try {
-    if (window.gtag) {
-      window.gtag('event', 'click', { event_category: 'outbound', event_label: label });
-    }
-  } catch {}
-}
